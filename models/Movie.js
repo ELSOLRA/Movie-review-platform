6fb39bddaa36f3cccaf9a2bf7ec9588bd8a4ba1f
ELSOLRA <bardakasmiljo@gmail.com>
2024-05-30 22:36:44 +0200

    data models ready-to-use

---
+++ models/Movie.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const movieSchema = mongoose.Schema({
+
+    title: {
+        type: String,
+        required: true
+    },
+    director: {
+        type: String,
+        required: true
+    },
+    releaseYear: {
+        type: Number,
+        required: true
+    },
+    genres: {
+        type: [String], 
+        required: true
+    },
+
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie; 
---
+++ models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // e-mail validation (regex) and \S matches all non-space characters
+        match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.']  
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true,
+        // standart role
+        default: 'user'
+    },
+    
+    
+});
+
+userSchema.pre('save', async function(next){
+    if(this.isModified('password')) {
+        try {
+            this.password = await bcrypt.hash(this.password, 10)
+        } catch (error) {
+            return next(error);
+        };    
+    };
+    next();
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User; 
